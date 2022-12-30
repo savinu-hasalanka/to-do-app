10e4e353c48b47c294f62d9287d01daf05e73e44
Savinu Hasalanka <savinu.hsa@gmail.com>
2022-12-30 15:57:51 +0530

    implement basic dynamic list

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+const items = [];
+
 app.get("/", (req, res) => {
-    res.send("<h1>Hi there!</h1>")
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    
+    const today = new Date();
+    const day = today.toLocaleDateString("us-en", options);
+
+    res.render("list", {kindOfDay: day, items: items});
+});
+
+app.post("/", (req, res) => {
+    const item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
 });
 
 
